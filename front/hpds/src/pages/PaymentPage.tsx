34232e0f77fc4b52e52a5243510c276c64052c10
ola567 <aleksandra.hein@o2.pl>
2024-05-16 23:06:17 +0200

    FRONT: payment page fixed

--- front/hpds/src/pages/PaymentPage.tsx
+++ front/hpds/src/pages/PaymentPage.tsx
@@ -1,44 +1,47 @@
 import NavBarLoggedIn from "../components/NavBarLoggedIn";
 import "../styles/FormStyles.css";
-import Button from "react-bootstrap/Button";
+import { Form, ButtonToolbar, Button, DateInput } from "rsuite";
 
 function PaymentPage() {
+  const handlePayment = (formData, event) => {
+    // Tutaj dodaj kod obsługujący płatność, np. wysłanie danych do serwera
+    console.log("Płatność została przetworzona:", formData);
+    // Możesz dodać tutaj również przekierowanie lub wyświetlenie komunikatu potwierdzającego
+  };
+
   return (
     <>
       <NavBarLoggedIn />
       <div className="container">
         <div className="header">Payment</div>
         <div className="inputs">
-          <div className="input">
-            <input
-              type="text"
-              placeholder="Credit card number"
-              name="credit-card-number"
-              required
-            />
-          </div>
-          <div className="input">
-            <input
-              type="text"
-              placeholder="Credit card expiration date"
-              name="credit-card-expiration-date"
-              required
-            />
-          </div>
-          <div className="input">
-            <input
-              type="text"
-              placeholder="Credit card security code"
-              name="credit-card-security-code"
-              required
-            />
-          </div>
-        </div>
-        <div className="submit-container">
-          <div className="time-left">Time left: 00:12</div>
-          <div className="buttons">
-            <Button variant="secondary">Pay</Button>
-          </div>
+          <Form onSubmit={handlePayment}>
+            <Form.Group>
+              <Form.ControlLabel>Credit card number:</Form.ControlLabel>
+              <Form.Control name="credit_card_number" />
+            </Form.Group>
+            <Form.Group controlId="datePicker">
+              <Form.ControlLabel>
+                Credit card expiration date:
+              </Form.ControlLabel>
+              <Form.Control
+                name="datePicker"
+                accepter={DateInput}
+                format="MM/yyyy"
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.ControlLabel>Credit card secure code:</Form.ControlLabel>
+              <Form.Control name="credit_card_secure_code" />
+            </Form.Group>
+            <Form.Group>
+              <ButtonToolbar>
+                <Button appearance="primary" type="submit">
+                  Pay
+                </Button>
+              </ButtonToolbar>
+            </Form.Group>
+          </Form>
         </div>
       </div>
     </>
