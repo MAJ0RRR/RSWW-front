35284c9cfacfeeb47fd6acbf76a209a7a36e1318
ola567 <aleksandra.hein@o2.pl>
2024-05-18 18:31:29 +0200

    FRONT: attached popular destinations

--- front/hpds/src/consts/consts.ts
+++ front/hpds/src/consts/consts.ts
@@ -1,2 +1,3 @@
 export const API_URL = "http://127.0.0.1:8000";
 export const LOGIN_ENDPOINT = "/Auth/Login";
+export const POPULAR_DESTINATIONS_ENDPOINT = "/Destinations/PopularOffers";
--- front/hpds/src/pages/HomePage.tsx
+++ front/hpds/src/pages/HomePage.tsx
@@ -1,15 +1,35 @@
 import NavBar from "../components/NavBar";
 import SearchBar from "../components/SearchBar";
 import Button from "react-bootstrap/Button";
+import { AxiosContextType } from "../axios/AxiosProvider";
+import AxiosContext from "../axios/AxiosProvider";
+import React, { useState, useEffect, useContext } from "react";
+import { POPULAR_DESTINATIONS_ENDPOINT } from "../consts/consts";
 
 function HomePage() {
-  // mocked variables
-  const mocked_popular_destinations = [
-    "Destination1",
-    "Destination2",
-    "Destination3",
-    "Destination4",
-  ];
+  const [popularDestinations, setPopularDestinations] = useState([]);
+  const { axiosInstance } = useContext(AxiosContext) as AxiosContextType;
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPopularDestinations = async () => {
+      try {
+        const response = await axiosInstance.get(
+          `${POPULAR_DESTINATIONS_ENDPOINT}`
+        );
+        setPopularDestinations(response.data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPopularDestinations();
+  }, []);
+
+  if (error) return <div>Something went wrong: {error.message}</div>;
 
   return (
     <>
@@ -17,18 +37,20 @@ function HomePage() {
       <SearchBar />
       <div className="page-content">
         <div className="page-title">Popular directions</div>
-        {mocked_popular_destinations.map((item) => (
-          <div className="page-section-content">
-            <div className="elements">
-              <div className="left-50 font-size-36">{item}</div>
-              <div className="right-50 font-size-36">
-                <Button variant="secondary" className="font-size-36">
-                  Check offers
-                </Button>
+        {loading && <div style={{ textAlign: "center" }}>Loading...</div>}
+        {!loading &&
+          popularDestinations.map((item) => (
+            <div className="page-section-content">
+              <div className="elements">
+                <div className="left-50 font-size-36">{item.country}</div>
+                <div className="right-50 font-size-36">
+                  <Button variant="secondary" className="font-size-36">
+                    Check offers
+                  </Button>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </>
   );
