11ba549f39f38b80ca622129d03151434477359b
Kepins <sz.kepinski1@gmail.com>
2024-05-20 01:57:54 +0200

    PRODUCTION: proxy pass to backend

--- front/hpds/src/consts/consts.ts
+++ front/hpds/src/consts/consts.ts
@@ -1,4 +1,4 @@
-export const API_URL = "http://apigateway:80";
+export const API_URL = "api";
 export const LOGIN_ENDPOINT = "/Auth/Login";
 export const POPULAR_DESTINATIONS_ENDPOINT = "/Destinations/PopularOffers";
 export const TOURS_ENDPOINT = "/Tours";
