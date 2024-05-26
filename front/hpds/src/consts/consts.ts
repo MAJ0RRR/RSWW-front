bcc23a8bcca931bf0dd358001c207ea00bd83bb3
ola567 <aleksandra.hein@o2.pl>
2024-05-27 00:56:42 +0200

    FRONT: fix searchbar

--- front/hpds/src/consts/consts.ts
+++ front/hpds/src/consts/consts.ts
@@ -1,6 +1,7 @@
 export const API_URL = "api";
 export const LOGIN_ENDPOINT = "/Auth/Login";
 export const POPULAR_DESTINATIONS_ENDPOINT = "/Destinations/PopularOffers";
+export const AVAILABLE_DESTINATIONS_ENDPOINT = "/Destinations/AvailableDestinations";
 export const TOURS_ENDPOINT = "/Tours";
 export const TRANSPORT_OPTION_ENDPOINT = "/TransportOptions";
 export const HOTEL_OPTION_ENDPOINT = "/Hotels";
